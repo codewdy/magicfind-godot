b05e2d2a4bd073929799c99939d96b8421b4636f
codewdy <codewdy@gmail.com>
2022-09-12 16:35:07 +0800

    add prefab

--- gdscript/main.lua
+++ gdscript/main.lua
@@ -6,7 +6,8 @@ local loader = require("gdscript.loader")
 
 function main:_enter_tree()
   loader:load()
-  require("ui.prefab"):load(loader:path():plus_file("ui/main.json"))
+  local p = require("ui.prefab"):load(loader:path():plus_file("prefab/main.json"))
+  self:add_child(p)
 end
 
 return main
--- script/ui/prefab.lua
+++ script/ui/prefab.lua
@@ -1,11 +1,43 @@
 local prefab = {}
 
+prefab.processor = {
+  type = function(obj, v)
+    -- Just Ignore
+  end,
+  script = function(obj, v)
+    -- Just Ignore
+  end,
+  name = function(obj, v)
+    obj:set_name(v)
+  end,
+  anchor = function(obj, v)
+    obj:set_anchor(GD.MARGIN_LEFT, v["left"], true)
+    obj:set_anchor(GD.MARGIN_RIGHT, v["right"], true)
+    obj:set_anchor(GD.MARGIN_TOP, v["top"], true)
+    obj:set_anchor(GD.MARGIN_BOTTOM, v["bottom"], true)
+  end,
+  margin = function(obj, v)
+    obj:set_margin(GD.MARGIN_LEFT, v["left"])
+    obj:set_margin(GD.MARGIN_RIGHT, v["right"])
+    obj:set_margin(GD.MARGIN_TOP, v["top"])
+    obj:set_margin(GD.MARGIN_BOTTOM, v["bottom"])
+  end,
+}
+
+function prefab:create_obj(json)
+  local result = _G[json["type"]:to_ascii()]:new()
+  for k,v in pairs(json) do
+    self.processor[k:to_ascii()](result, v)
+  end
+  return result
+end
+
 function prefab:load(filename)
   local file = io.open(filename:to_utf8(), "r")
-  local jsonstr = file:read()
-  print(jsonstr)
-  print(JSON:parse(jsonstr))
+  local jsonstr = file:read("a")
   file:close()
+  local result = JSON:parse(jsonstr):get_result()
+  return self:create_obj(result)
 end
 
 return prefab
